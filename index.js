e73d6196dc60b2a073928e9b12fa55a75048fdff
seron <seroncj@gmail.com>
2022-08-28 16:12:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+  function reverseString(word) {
+    const wordArray = word.split("");
+    const reversedWordArray = wordArray.reverse();
+    const reversedWord = reversedWordArray.join("");
+    return reversedWord;
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
